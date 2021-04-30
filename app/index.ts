a17e4e6bf1b4bcfea2c822c5cda8138a732a6026
saphirevert <jerome.cosandey@gmail.com>
2021-04-30 14:28:33 +0200

    Non au code mort

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,5 @@
 import { Telegraf, Markup } from 'telegraf';
-import { Controller } from './controller/Controller';
-import { Users } from './controller/Users';
 import { Commands } from './lib/Commands';
-// import { User } from './model/User';
-// import { createTextChangeRange } from 'typescript';
 import secrets from './secrets.json';
 
 const BOT_TOKEN: string = secrets.BOT_TOKEN;
@@ -11,23 +7,4 @@ const BOT_OWNER_ID: string = secrets.BOT_OWNER_ID;
 const bot = new Telegraf(BOT_TOKEN);
 
 var commands = new Commands(bot, secrets);
-// console.log("hello world");
-
-// Users.list[1317042017].settings.isAdmin = false;
-// Users.save();
-// Users.test();
-console.log("------");
-
-// Users.list[1317042017].settings.isAdmin = true;
-// Users.save();
-// Users.setPrivilegeStatus("1317042017", true);
-// Users.addUser("1317042017");
-
-// console.log(bot.);
-
-// bot.hears(new RegExp(`(^\/latest)`), async (ctx) => {
-//     let props = ctx.update.message.text.split(" ");
-//     let count = parseInt(props[1]);
-//     ctx.reply("toto");
-// });
 commands.load();
--- app/lib/Commands.ts
+++ app/lib/Commands.ts
@@ -15,26 +15,14 @@ export class Commands {
     }
 
     async load(){
-        // console.log(Markup.button.callback('Admin request', 'admin_request'));
-
-        // this.menu();
         this.onCallbackQuery();
-        this.mainMenu();
-        this.inlineCommand();
-        this.start();
-        this.launch();
         this.onText();
+        this.launch();
         return true;
-    }
-
-
-    async test(ctx:any){
-        ctx.reply(ctx.message.text)
     }
 
     async inlineCallbackKeyboard(chat_id: string, inline_message: string, keyboard: Array<any>){
         let inlineKeyboard: Array<any> = [];
-        
         for (let i = 0; i < keyboard.length; i++) {
             inlineKeyboard[i] = [];
             for (let j = 0; j < keyboard[i].length; j++) {
@@ -42,22 +30,18 @@ export class Commands {
                 inlineKeyboard[i][j] = Markup.button.callback(keyboard[i][j][0], keyboard[i][j][1], keyboard[i][j][2]);
             }
         }
-        
         const test = [[{ text: '👍', callback_data: ':smiley:', hide: false }]];
         return this.bot.telegram.sendMessage(chat_id, inline_message, {
             parse_mode: 'HTML',
             ...Markup.inlineKeyboard(inlineKeyboard)
-          })
+        })
     }
 
-
-    
     async menu(ctx:any){
             let userFromId = ctx.update.message.chat.id;
             Users.check(userFromId);
             let isNotAdmin = Users.list[userFromId].settings.isAdmin;
             let isAdmin = Users.list[userFromId].settings.isAdmin ? false : true;
-            
             this.inlineCallbackKeyboard(userFromId, "Welcome ! What do you wants to do ?", 
                 [
                     [["Admin request", "admin_request", isNotAdmin], ["New post", "new_post", isAdmin]],
@@ -67,10 +51,6 @@ export class Commands {
     }
 
     async onText(){
-        // console.log(await this.bot.telegram.getChatMember(976140946, 976140946));
-        // console.log(await this.bot.telegram.getChatMembersCount(976140946));
-        // var test = new dataModel();
-        // test.createUser("976140946");
         return this.bot.on('text', (ctx: any) => {
             console.log(ctx.update.message.from);
             console.log(ctx.message.text);
@@ -79,34 +59,17 @@ export class Commands {
             
             switch (true) {
                 case new RegExp(`(^\/test)(@${this.bot.botInfo?.username})?$`).test(commandArray[0]): this.test(ctx); break;  
-                    
-                
                 case new RegExp(`(^\/menu)(@${this.bot.botInfo?.username})?$`).test(commandArray[0]): this.menu(ctx); break;
-                
-                
-                case new RegExp(`(^\/testreg)`).test(ctx.message.text):
-                    console.log("testreg works");
-                    
-                    break;
-            
-                default:
-                    break;
-            }
-            
-        })
-          
-    }
-
-    async newPost(message: string){
-        return this.bot.telegram.sendMessage(this.secrets.BOT_OWNER_ID, message + "\n\nRejoignez-nous")
+                case new RegExp(`(^\/start)(@${this.bot.botInfo?.username})?$`).test(commandArray[0]): this.start(ctx); break;
+                default: break;
+            }  
+        })   
     }
 
     async onCallbackQuery(){
         return this.bot.on('callback_query', async (ctx: any) => {
             var callbackUserFrom = ctx.update.callback_query.from
             console.log(ctx.update.callback_query.data);
-            // console.log(ctx.update.callback_query.message.reply_markup.inline_keyboard);
-            // console.log(ctx.update.callback_query.message);
             var queryData:string = ctx.update.callback_query.data;
             var command = queryData.split(" ");
             switch(command[0]){
@@ -117,7 +80,6 @@ export class Commands {
                         this.inlineCallbackKeyboard(this.secrets.BOT_OWNER_ID, message, [[["Yes", `setadmin ${callbackUserFrom.id} true`], ["No", `setadmin ${callbackUserFrom.id} false`]]]);
                         // return ctx.telegram.sendMessage(this.secrets.BOT_OWNER_ID, message);
                     } else {
-                         
                         return ctx.reply("You are the owner of the channel...")
                     }
                     break;
@@ -147,45 +109,16 @@ export class Commands {
         })
     }
 
-    async mainMenu(){
-        return this.bot.command('custom', async (ctx) => {
-            return await ctx.reply('Custom buttons keyboard', Markup
-              .keyboard([
-                ['🔍 Search', '😎 Popular'], // Row1 with 2 buttons
-                ['☸ Setting', '📞 Feedback'], // Row2 with 2 buttons
-                ['📢 Ads', '⭐️ Rate us', '👥 Share'] // Row3 with 3 buttons
-              ])
-              .oneTime()
-              .resize()
-            )
-        })
-    }
-    async inlineCommand(){
-        return this.bot.command('inline', (ctx) => {
-            return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', {
-              parse_mode: 'HTML',
-              ...Markup.inlineKeyboard([[
-                  Markup.button.callback('👍', ':smiley:'),
-                  // Markup.button.callback('🙂', ':slightly_smiling_face:'),
-                  Markup.button.callback('👎', ':slightly_frowning_face:'),
-                ],
-                [
-                  Markup.button.callback('😃 🟦🟦🟦🟦🟦🟥🟥🟥🟥🟥 🙁', ':smiley:'),
-                ]
-              ])
-            })
-        })
-    }
-
-    async start(){
-        return this.bot.start((ctx: any) => {
+    async start(ctx:any){
             Users.check(ctx.message.from.id);
             ctx.reply('Welcome !');
-        })
     }
 
     async launch(){
         return this.bot.launch();
     }
 
+    async test(ctx:any){
+        ctx.reply(ctx.message.text)
+    }
 }
